1e9815e6de078b2dca07da5297c2fc7269c5fa76
fahlantt <mhammadfahlanandar@gmail.com>
2025-06-19 00:34:45 +0800

    update koneksi database ke Railway + install dotenv

--- db.js
+++ db.js
@@ -1,10 +1,13 @@
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '', // sesuaikan kalau kamu pakai password
-  database: 'task-flow',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 export default db;
